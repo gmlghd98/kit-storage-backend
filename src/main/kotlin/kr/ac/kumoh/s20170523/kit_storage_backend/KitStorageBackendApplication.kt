35f580e39ef73f8751041530b180d2e63d69c282
heehong <gmlghd98@gmail.com>
2024-12-12 13:32:44 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/KitStorageBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20170523.kit_storage_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitStorageBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<KitStorageBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/KitStorageBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20170523.kit_storage_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KitStorageBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
