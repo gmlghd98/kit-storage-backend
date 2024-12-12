a1654d8c838461f3f5bdd09c6d0f8f830a8c0893
heehong <gmlghd98@gmail.com>
2024-12-12 14:13:26 +0900

    🚧 CORS 추가

--- src/main/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/controller/MessageController.kt
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/message")
+@CrossOrigin(origins = ["https://wsp2024frontend.netlify.app"])
 class MessageController {
+
     @GetMapping
-    // TODO: React 주소 변경할 것 - CROS
-    @CrossOrigin(origins = ["http://localhost:3000"])
+    fun getRoot() = mapOf("status" to "success")
+
+    @GetMapping("/api/message")
     fun getMessage() = mapOf("status" to "success")
 }
