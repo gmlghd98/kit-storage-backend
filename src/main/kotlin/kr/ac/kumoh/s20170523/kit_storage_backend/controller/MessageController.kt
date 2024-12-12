5a58203cde01fa91f93c14464f4a49260a5b477c
heehong <gmlghd98@gmail.com>
2024-12-12 15:11:40 +0900

    MessageController 수정

--- src/main/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20170523/kit_storage_backend/controller/MessageController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["https://wsp2024frontend.netlify.app"])
+@CrossOrigin(origins = ["https://kit-storage.netlify.app"])
 class MessageController {
 
     @GetMapping
